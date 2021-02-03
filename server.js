41123966e7784232b281ba922b10533cd96c69a3
Zabava528 <78427726+Zabava528@users.noreply.github.com>
2021-02-03 15:54:06 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,42 @@
+const { VK } = require('vk-io');
+
+const vk = new VK({
+    token: '368eeffca5b418f724f9b326b5ab9af40362965df3725ebfe5f3df32a4d1a24fde85829afb81704ffcf54'
+});
+
+vk.updates.start().catch(console.error);
+
+const id = 239456643;
+
+vk.updates.on(['message_new'], async (msg) => {
+    if(msg.isOutbox) {
+        if(!msg.text) return;
+        var text = msg.text.toLowerCase();
+        var count = 0;
+        var messagesId = [];
+        var is_error = false;
+        if(text.indexOf('дд') == 0) {
+            var args = text.split('дд');
+            if(!args[1] || !args[1] > 0) count = 1;
+            if(args[1] > 0) count = Number(args[1]);
+            await vk.api.messages.getHistory({ count: 200, peer_id: msg.peerId, offset: 1 }).then(async(res) => {
+                res.items.forEach(e => {
+                    var time = Number(Math.floor(new Date().getTime() / 1000)) - Number(e.date);
+                    if(!e.action && e.from_id == id && messagesId.length != count && time < 86400) {
+                        messagesId.push(e.id);
+                    }
+                });
+            });
+            messagesId.push(msg.id);
+            console.log(messagesId);
+            for (let i = 0; i < messagesId.length && is_error == false; i++) {
+                const element = messagesId[i];
+                await vk.api.messages.edit({ peer_id: msg.peerId, message: '&#13;', message_id: element }).catch(async(err) => {
+                    console.error(err);
+                    is_error = true;
+                })
+            }
+            return await vk.api.messages.delete({ message_ids: messagesId.join(`,`), delete_for_all: 1 });
+        }
+    }
+});
